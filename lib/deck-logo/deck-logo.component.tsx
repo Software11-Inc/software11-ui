1e90ddadd55cfe53ecff1cce2009698f0e6d15fc
Valentyn Shtanko <valentyn.shtanko@gmail.com>
2024-07-03 03:00:50 +0200

    refactor: Rename Logo component to DeckLogo

--- lib/deck-logo/deck-logo.component.tsx
+++ lib/deck-logo/deck-logo.component.tsx
@@ -2,7 +2,7 @@ import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
 import { boxStyle, typographyStyle1, typographyStyle2 } from "./deck-logo.styles";
 
-export const Logo = (): React.ReactElement => (
+export const DeckLogo = (): React.ReactElement => (
   <Box sx={boxStyle}>
     <Typography sx={typographyStyle1}>Deck</Typography>
     <Typography sx={typographyStyle2}>craft</Typography>
