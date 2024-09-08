d5719625e6104bd8d725a7f91f3bea43b7dea6d8
Valentyn Shtanko <valentyn.shtanko@gmail.com>
2024-09-08 13:35:01 +0200

    chore: Update tab mask size in deck-footer-navigation component

--- lib/deck-footer-navigation/tab-mask.icon.tsx
+++ lib/deck-footer-navigation/tab-mask.icon.tsx
@@ -4,16 +4,12 @@ export const TabMaskIcon: React.FC<{}> = () => (
   <svg width={"4rem"} height={"1.5rem"}>
     <defs>
       <mask id="mask-tab">
-        <svg viewBox="0.25 0 3.5 1.5" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M 0 -0.5 A 1 1 0 0 1 1 0 A 1 1 0 0 0 3 0 A 1 1 0 0 1 4 -0.5 V -1 H 0 V -0.5 Z"
-            fill="black"
-            stroke="black"
-          />
+        <svg viewBox="0 0.5 40 15" xmlns="http://www.w3.org/2000/svg">
+          <path d="M 0 0 C 5 0 5 2 6 5 A 15 15 0 0 0 34 5 C 35 2 35 0 40 0 Z" fill="black" stroke="black" />
         </svg>
       </mask>
       <mask id="mask-outer" maskContentUnits="objectBoundingBox">
-        <rect x="0" y="0" width="1" height="1"></rect>
+        <rect x="0" y="0" width="40" height="15"></rect>
       </mask>
     </defs>
   </svg>
