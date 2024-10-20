a598d9a95df7041a65d2592f6af1d3070be65d8f
Valentyn Shtanko <valentyn.shtanko@gmail.com>
2024-10-20 21:08:01 +0200

    Added story for template preview

---
+++ lib/deck-template-preview/deck-template-preview.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { DeckTemplatePreview } from "./deck-template-preview.component";
+
+export default {
+  title: "Deck Template Preview",
+  component: DeckTemplatePreview,
+} as Meta<typeof DeckTemplatePreview>;
+
+const Template: StoryFn<typeof DeckTemplatePreview> = (args) => <DeckTemplatePreview {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  item: {
+    name: "Template Name",
+    description: "Template Description",
+  },
+};
