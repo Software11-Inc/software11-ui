22a3c27589e653cb76640da54f609e063fe5dac5
Valentyn Shtanko <valentyn.shtanko@gmail.com>
2024-10-29 16:44:50 +0100

    chore: Update deck-file-group-item.types.tsx
    Refactor the IDeckFileGroupItemProps interface in deck-file-group-item.types.tsx to make the itemTemplate prop optional. This change allows for more flexibility when using the DeckFileGroupItem component by allowing the itemTemplate to be omitted if needed.

--- lib/deck-file-group-item/deck-file-group-item.types.tsx
+++ lib/deck-file-group-item/deck-file-group-item.types.tsx
@@ -3,6 +3,6 @@ import { IFile, IFileSheet } from "@models";
 export interface IDeckFileGroupItemProps {
   file: IFile;
   defaultExpanded?: boolean;
-  itemTemplate: (item: IFileSheet) => JSX.Element;
+  itemTemplate?: (item: IFileSheet) => JSX.Element;
   emptyTemplate?: JSX.Element;
 }
---
+++ lib/deck-repeater-preview/deck-repeater-preview.types.ts
@@ -0,0 +1,4 @@
+export interface DeckRepeaterPreviewProps {
+  title: string;
+  description: string;
+}
