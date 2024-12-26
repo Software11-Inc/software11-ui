f27137ff95ed89ef07b2a49559b7ad0bfd90ae33
Valentyn Shtanko <valentyn.shtanko@gmail.com>
2024-12-26 04:01:32 +0100

    Refactor notification model and story types

--- lib/models/notification.model.ts
+++ lib/models/notification.model.ts
@@ -1,3 +1,29 @@
+import { IDefaultItem } from "./default-item.model";
+
 export type DeckSnackbarTextIcon = "success" | "error" | "warning" | "info";
 
-export const deckSnackbarTextIcon: DeckSnackbarTextIcon[] = ["success", "error", "warning", "info"];
+export type DeckNotificationSource = "web" | "powerpoint" | "excel";
+
+export type DeckNotificationAction = "create" | "update" | "delete" | "upload";
+
+export type DeckNotificationObjectType = "dataset" | "slide" | "project" | "template" | "image" | "album" | "folder";
+
+export interface INotification extends IDefaultItem {
+  source?: {
+    application: DeckNotificationSource;
+  };
+
+  action?: DeckNotificationAction;
+
+  objectType?: DeckNotificationObjectType;
+
+  objectId?: string;
+
+  objectName?: string;
+
+  isRead?: boolean;
+
+  severity?: DeckSnackbarTextIcon;
+
+  textIcon?: DeckSnackbarTextIcon;
+}
--- lib/story-types.ts
+++ lib/story-types.ts
@@ -1,3 +1,5 @@
+import { DeckSnackbarTextIcon } from "./models/notification.model";
+
 export const sizes = ["sm", "md", "lg"];
 
 export const colors = ["primary", "neutral", "danger", "warning", "success"];
@@ -5,3 +7,5 @@ export const colors = ["primary", "neutral", "danger", "warning", "success"];
 export const variants = ["plain", "soft", "outlined", "solid"];
 
 export const datasetTypes = ["default", "excel-table", "excel-matrix", "excel-default"];
+
+export const deckSnackbarTextIcon: DeckSnackbarTextIcon[] = ["success", "error", "warning", "info"];
