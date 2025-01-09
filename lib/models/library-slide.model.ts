96c8886949a1e4cab98cc2d6cd9cdd23d3b5b04f
Valentyn Shtanko <valentyn.shtanko@gmail.com>
2025-01-09 19:31:36 +0100

    feat: Add lastUpdateUser property to ILibrarySlide interface

--- lib/models/library-slide.model.ts
+++ lib/models/library-slide.model.ts
@@ -1,5 +1,6 @@
 import { ISourceProject } from "./dataset-tree.model";
 import { IDefaultItem } from "./default-item.model";
+import { IUser } from "./user.model";
 
 /**
  * Interface representing a library slide, extending the default item properties.
@@ -9,6 +10,7 @@ export interface ILibrarySlide extends IDefaultItem {
   previewImageURL?: string; // Optional URL for the slide's preview image
   storedFileURL?: string; // Optional URL where the slide's file is stored
   sourceProject?: ISourceProject; // The project where the slide is sourced from
+  lastUpdateUser?: IUser; // Thanks backend for 3 different user properties
 }
 
 /**
